49af238892547484f1919014d10f54e7c416952e
revo <luisdanker@gmx.de>
2024-07-31 14:31:42 +0200

    improvments

--- lua/revo/plugins/bufferline.lua
+++ lua/revo/plugins/bufferline.lua
@@ -14,6 +14,11 @@ return {
     keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
     keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
     keymap.set("n", "<leader>x", "<cmd>BufferLinePickClose<CR>")
+    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+      callback = function()
+        vim.api.nvim_command_output("BufferLineSortByDirectory")
+      end,
+    })
   end,
 
 }
