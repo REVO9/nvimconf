78c50cc19a7bc2283122cc62662318bc72f99f3b
revo <luisdanker@gmx.de>
2024-08-01 19:35:00 +0200

    undotree

--- lua/revo/plugins/bufferline.lua
+++ lua/revo/plugins/bufferline.lua
@@ -14,7 +14,7 @@ return {
     keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
     keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
     keymap.set("n", "<leader>x", "<cmd>BufferLinePickClose<CR>")
-    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete", "BufEnter" }, {
       callback = function()
         vim.api.nvim_command_output("BufferLineSortByDirectory")
       end,
--- lua/revo/plugins/telescope.lua
+++ lua/revo/plugins/telescope.lua
@@ -1,12 +1,33 @@
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    "debugloop/telescope-undo.nvim",
+  },
+
+  opts = {
+    extensions = {
+      undo = {
+      },
+    },
+  },
 
   config = function()
+    require("telescope").setup(opts)
+    require("telescope").load_extension("undo")
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find Files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live Grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'List Buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help Tags' })
-  end
+
+    local undo = require("telescope-undo.actions")
+    vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>", { desc = "undo history" })
+
+    vim.keymap.set('i', '<cr>', undo.yank_additions, { desc = 'Yank Additions' })
+    vim.keymap.set('i', '<S-cr>', undo.yank_deletions, { desc = 'Yank Deletions' })
+    vim.keymap.set('i', '<C-cr>', undo.restore, { desc = 'Restore' })
+  end,
 }
