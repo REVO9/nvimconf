2b955f9be0dbfbd820cc1227c8597d2b449aee34
revo <luisdanker@gmx.de>
2024-09-03 01:31:39 +0200

    more keybinds

--- lua/revo/plugins/bufferline.lua
+++ lua/revo/plugins/bufferline.lua
@@ -13,12 +13,17 @@ return {
 
     keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
     keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
-    keymap.set("n", "<leader>x", "<cmd>BufferLinePickClose<CR>")
-    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete", "BufEnter" }, {
-      callback = function()
-        vim.api.nvim_command_output("BufferLineSortByDirectory")
-      end,
-    })
+    keymap.set("n", "<leader>bx", "<cmd>BufferLinePickClose<CR>", {desc = "Close a buffer"})
+    keymap.set("n", "<leader>bc", "<cmd>BufferLinePick<CR>", {desc = "Choose a buffer"})
+    keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<CR>", {desc = "Close all other buffers"})
+    keymap.set("n", "<leader>bs", "<cmd>BufferLineSortByDirectory<CR>", {desc = "Sort buffers"})
+    keymap.set("n", "<leader>bp", "<cmd>BufferLineTogglePin<CR>", {desc = "Pin buffer"})
+
+    -- vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete", "BufEnter" }, {
+    --   callback = function()
+    --     vim.api.nvim_command_output("BufferLineSortByDirectory")
+    --   end,
+    -- })
   end,
 
 }
